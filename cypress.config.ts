2d88914aa4a5a03a29e1614ea6e2bc805cbd2749
Carlos Vigo <cvigo@baufest.com>
2025-10-20 22:47:05 -0300

    Enhance Cypress configuration with mochawesome reporter and update TypeScript settings in tsconfig.json

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,32 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  chromeWebSecurity:false,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
+    // baseUrl: process.env.CYPRESS_HOST || 'https://concntric.com/',
+    viewportWidth: 1920,
+    viewportHeight: 1080,
+    defaultCommandTimeout: 10000,
+    requestTimeout: 10000,
+    responseTimeout: 10000,
+    video: true,
+    videosFolder: 'cypress/reports/html/videos',
+    screenshotOnRunFailure: true,
+    screenshotsFolder: 'cypress/reports/html/screenshots',
+    chromeWebSecurity: false,
+    reporter: 'cypress-mochawesome-reporter',
+    reporterOptions: {
+      charts: true,
+      reportPageTitle: 'Concntric Test Report',
+      embeddedScreenshots: true,
+      inlineAssets: false,
+      saveAllAttempts: false,
+      reportDir: 'cypress/reports/html',
+    },
+
   },
 });
